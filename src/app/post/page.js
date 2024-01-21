71518844eb635711de37f21a7b2cec208ca76b43
sajalbiswas1 <sajalb1899@gmail.com>
2024-01-21 22:51:25 +0600

    data fatch to show ui

---
+++ src/app/post/page.js
@@ -0,0 +1,26 @@
+
+
+const PostPage = async() => {
+    const res = await fetch("http://localhost:5000/post");
+    const posts = await res.json();
+    console.log(posts)
+    return (
+        <div className="w-full">
+            <h1>This is a post page: {posts.length}</h1>
+            {
+                posts.map(post => <div key={post.id} className="card w-[70%] mx-auto my-5 bg-gray-100 shadow-xl">
+                <div className="card-body">
+                  <h2 className="card-title">{post.title}</h2>
+                  <p>{post.description}</p>
+                  <h2 className="card-title">Like: {post.count}</h2>
+                  <div className="card-actions justify-end">
+                    <button className="btn btn-primary">See more</button>
+                  </div>
+                </div>
+              </div>)
+            }
+        </div>
+    );
+};
+
+export default PostPage;
