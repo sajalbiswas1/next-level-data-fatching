f6265bd1e5c2d0486c0665ac2e16376e5e3133a2
sajalbiswas1 <sajalb1899@gmail.com>
2024-01-22 09:15:14 +0600

    api route

---
+++ src/app/api/blogs/route.js
@@ -0,0 +1,19 @@
+import { NextResponse } from "next/server"
+
+
+export const GET = () =>{
+    return NextResponse.json({message: "Hello World"})
+}
+
+// post api
+export const POST = ()=>{
+
+}
+// patch api
+export const PATCH = ()=>{
+
+}
+// Delete api
+export const DELETE = ()=>{
+
+}
